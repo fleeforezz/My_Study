a9a9af9d866f77d2b4dd2b4f5d8c86b93ebf1b1a
Trương Minh Nhật <fleeforezz@gmail.com>
2025-06-03 15:15:09 +0700

    Update

---
+++ .NET/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            if (x <= y)
+            {
+                Console.WriteLine("Hello World");
+                int sum = x + y;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
