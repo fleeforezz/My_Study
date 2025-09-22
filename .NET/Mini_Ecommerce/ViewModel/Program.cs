1bd6b84318911c7e7ccdf35f1ec5292cc15aa3b6
Trương Minh Nhật <fleeforezz@gmail.com>
2025-09-22 09:02:15 +0700

    Update

--- .NET/Mini_Ecommerce/ViewModel/Program.cs
+++ .NET/Mini_Ecommerce/ViewModel/Program.cs
@@ -15,9 +15,59 @@ namespace ViewModel
                 Console.WriteLine("1. Product Actions");
                 Console.WriteLine("2. Register Customers");
                 Console.WriteLine("3. Place an Order");
-                Console.WriteLine("4. ");
+                Console.WriteLine("4. Show order history for customer");
+                Console.WriteLine("0. Exit");
+                Console.Write("Select choice: ");
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        int productChoice = 0;
+
+                        do
+                        {
+                            Console.WriteLine("1. Add new Product");
+                            Console.WriteLine("2. List all product");
+                            Console.WriteLine("3. Update product");
+                            Console.WriteLine("4. Delete product");
+                            Console.WriteLine("0. Return to main menu");
+                            Console.Write("Select choice: ");
+                            productChoice = int.Parse(Console.ReadLine());
+
+                            switch (productChoice)
+                            {
+                                case 1:
+
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+                                case 4:
+                                    break;
+                                case 0:
+                                    return;
+                                default:
+                                    Console.WriteLine("Invalid choice!!!");
+                                    break;
+                            }
+                        } while (choice > 0 && choice < 5);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice!!!");
+                        break;
+                }
             }
-            while (choice >= 0 && choice < 5);
+            while (choice > 0 && choice < 5);
         }
     }
 }
