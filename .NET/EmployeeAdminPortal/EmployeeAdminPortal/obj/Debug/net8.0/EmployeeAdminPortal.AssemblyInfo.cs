c692a6bdd9367181b29a7a4ec545208f6b323c3f
Trương Minh Nhật <fleeforezz@gmail.com>
2025-10-10 09:54:31 +0700

    Update

--- .NET/EmployeeAdminPortal/EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
+++ .NET/EmployeeAdminPortal/EmployeeAdminPortal/obj/Debug/net8.0/EmployeeAdminPortal.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0efb4388d246af6810c22dd54f63c5aed9efd30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46b56fefd79b0db2464d0d349b3669e9f7362ec6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
