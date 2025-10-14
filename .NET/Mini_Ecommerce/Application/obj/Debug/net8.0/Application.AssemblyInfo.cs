fbfe346b98cdf2b0e67ddb77ff3471faf36cc27c
Trương Minh Nhật <fleeforezz@gmail.com>
2025-10-14 18:14:37 +0700

    Update

--- .NET/Mini_Ecommerce/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ .NET/Mini_Ecommerce/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844ea1ea83bc0a135dac4d49f1ee9ec35f548493")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1c19a45e2b5afff9ed970a2c48dffd066017b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ .NET/Mini_Ecommerce/UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using UI.Views;
+
+namespace UI.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ShowDashboardCommand { get; }
+        public ICommand ShowProductCommand { get; }
+        public ICommand ShowCustomerCommand { get; }
+        public ICommand ShowOrderCommand { get; }
+        public ICommand ShowReportCommand { get; }
+
+        public MainViewModel()
+        {
+            // Init default view
+            CurrentView = new DashboardView();
+
+            // Create commands
+            ShowDashboardCommand = new RelayCommand(_ => CurrentView = new DashboardView());
+            ShowProductCommand = new RelayCommand(_ => CurrentView = new ProductView());
+            ShowCustomerCommand = new RelayCommand(_ => CurrentView = new CustomerView());
+            ShowOrderCommand = new RelayCommand(_ => CurrentView = new OrderView());
+            ShowReportCommand = new RelayCommand(_ => CurrentView = new ReportView());
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
