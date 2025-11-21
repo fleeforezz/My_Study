3ba3aab018a0f6e4f749721afdd27d45f06ae341
Trương Minh Nhật <fleeforezz@gmail.com>
2025-11-21 20:51:48 +0700

    Update

---
+++ .NET/MyFirstWinUiApp/MyFirstWinUiApp/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstWinUiApp
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public partial int Count {  get; set; }
+
+        [RelayCommand]
+        public void InrementCount()
+        {
+            Count++;
+        }
+    }
+}
