56a3eecd9d8689386fe580fac875a5936b985cb0
Trương Minh Nhật <fleeforezz@gmail.com>
2026-01-19 11:02:23 +0700

    Update

--- SpringBoot/DLS/src/main/java/com/group4/DLS/controller/UserController.java
+++ SpringBoot/DLS/src/main/java/com/group4/DLS/controller/UserController.java
@@ -13,7 +13,6 @@ import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
--- SpringBoot/DLS/src/main/java/com/group4/DLS/service/UserService.java
+++ SpringBoot/DLS/src/main/java/com/group4/DLS/service/UserService.java
@@ -14,7 +14,6 @@ import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
